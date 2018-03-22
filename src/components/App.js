8b6a77ef842fb91a92001cb0d89f8000a8c5a021
Marta Aguilar <martaguilar.soto@gmail.com>
2018-03-22 14:16:29 +0100

    add structure editor

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,11 @@ export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state ={
+      ArrDays: [],
+      ArrInfo: [],
+      date: '',
+      message: ''
+
 
 
     };
@@ -16,8 +21,19 @@ export default class App extends React.Component {
         <header className="App-header">
 
         </header>
+        <form action="">
+        <h2>Fecha</h2>
+        <input id="date" type="date"></input>
 
+        <h2>Estado</h2>
+        <input type="checkbox" name="smile" value="smile" /> :)
+        <input type="checkbox" name="angry" value="angry" /> :(
 
+        <h2>Mensaje</h2>
+        <input type="text" placeholder="¿Por qué es un buen día?"></input>
+        <input type="button" value="Guardar" />
+        <input type="button" value="Cancelar" />
+        </form>
       </div>
     );
   }
